a6d9921511687b56c4a97760890b192dccd019bd
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2020-09-01 12:39:05 +0200

    Initial Commit

---
+++ Projects/BrainPad_TinyCLR_V1_Blinky/BrainPad_TinyCLR_V1_Blinky/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using GHIElectronics.TinyCLR.BrainPad;
+using System.Diagnostics;
+
+
+namespace BrainPad_TinyCLR_V1_Blinky
+{
+    class Program
+    {
+        static LightBulb lightBulb = new LightBulb();
+        static int counter = 0;
+        static void Main()
+        {
+            Thread.Sleep(1000);
+            while (true)
+            {
+                Thread.Sleep(1000);
+                lightBulb.TurnBlue();
+                counter++;
+                Debug.WriteLine("Hello TinyCLR " + counter);
+                Thread.Sleep(1000);
+                lightBulb.TurnGreen();
+            }
+        }
+    }
+}
